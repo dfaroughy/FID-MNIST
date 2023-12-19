3e0f0f6617c649ba1adfb645f5427a52fa3fbf07
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-12-19 11:37:06 -0500

    update

--- fid.py
+++ fid.py
@@ -4,22 +4,6 @@ import numpy as np
 from torch.utils.data import DataLoader
 
 from datasets import load_pepper_mnist, load_noisy_pepper_mnist, load_blurred_pepper_mnist, load_swirled_pepper_mnist
-
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = torch.relu(torch.max_pool2d(self.conv1(x), 2))
-        x = torch.relu(torch.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 320)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return torch.log_softmax(x, dim=1)
     
 
 class FeatureExtractor(nn.Module):
--- utils.py
+++ utils.py
@@ -4,6 +4,52 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import torch.nn as nn 
 
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = torch.relu(torch.max_pool2d(self.conv1(x), 2))
+        x = torch.relu(torch.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 320)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return torch.log_softmax(x, dim=1)
+
+class LeNet(torch.nn.Module):
+    def __init__(self):
+        
+        super(LeNet, self).__init__()
+        # feature extractor CNN
+        self._feature_extractor = torch.nn.Sequential(
+            torch.nn.Conv2d(1,6,5),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2,2),
+            torch.nn.Conv2d(6,16,5),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2,2) )
+        # classifier MLP
+        self._classifier = torch.nn.Sequential(
+            torch.nn.Linear(256,120),
+            torch.nn.ReLU(),
+            torch.nn.Linear(120,84),
+            torch.nn.ReLU(),
+            torch.nn.Linear(84,10) )
+
+    def forward(self, x):
+        # extract features
+        features = self._feature_extractor(x)
+        # flatten the 3d tensor (2d space x channels = features)
+        features = features.view(-1, np.prod(features.size()[1:]))
+        # classify and return
+        return self._classifier(features)
+
+
 def get_accuracy_confusion_matrix(model, device, test_loader):
     model.to(device)
     model.eval()
