cf7742fd042789098d9a2cb3f6c9096f092d04a1
Darius Faroughy <dariusfar@gmail.com>
2024-04-10 16:41:46 -0400

    update

--- architectures.py
+++ architectures.py
@@ -110,27 +110,3 @@ class ResBlock(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
-
-
-#...old
-
-# class LeNet5(nn.Module):
-#     def __init__(self, num_classes):
-#         super(LeNet5, self).__init__()
-#         self.conv1 = nn.Conv2d(1, 6, 5)
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-#         self.fc1 = nn.Linear(16 * 4 * 4, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, num_classes)
-
-#     def forward(self, x, activation_layer=None):
-#         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-#         x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
-#         x = x.view(-1, np.prod(x.size()[1:]))
-#         x = F.relu(self.fc1(x))
-#         if activation_layer == 'fc1': return x  
-#         x = F.relu(self.fc2(x))
-#         if activation_layer == 'fc2': return x 
-#         x = self.fc3(x)  
-#         if activation_layer == 'fc3': return x  
-#         return F.log_softmax(x, dim=1)  
